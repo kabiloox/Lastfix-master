a833f609ff66d7386067a82acf8a3e4af68f3aa9
kabiloox <37252571+kabilox@users.noreply.github.com>
2018-03-21 01:38:34 +0000

    base

---
+++ Electronique_Labo/Migrations/201803210127366_initialm.cs
@@ -0,0 +1,19 @@
+namespace Electronique_Labo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialm : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+            ;
+           
+        }
+    }
+}
---
+++ Electronique_Labo/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Electronique_Labo.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Electronique_Labo.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Electronique_Labo.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
